f802a60c3cf1744181564e47641d3664a4d2e67a
Sarai Díaz García <dgarciasarai@gmail.com>
2018-06-11 10:37:01 +0200

    Create framework target

---
+++ LiferayPush/LiferayPush.h
@@ -0,0 +1,19 @@
+//
+//  LiferayPush.h
+//  LiferayPush
+//
+//  Created by Sarai Díaz on 11/06/2018.
+//  Copyright © 2018 Bruno Farache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiferayPush.
+FOUNDATION_EXPORT double LiferayPushVersionNumber;
+
+//! Project version string for LiferayPush.
+FOUNDATION_EXPORT const unsigned char LiferayPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiferayPush/PublicHeader.h>
+
+
